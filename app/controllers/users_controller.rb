1e6e8924497e23489039f6035907167f762285a5
crop0422 <ayumi.yamori@gmail.com>
2015-11-18 17:02:39 +0000

    updated user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,18 +18,21 @@ class UsersController < ApplicationController
   end
   
   def edit
+    @user = User.find(params[:id])
   end
   
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to root_path, notice: 'Updated profile'
+      flash[:success] = "Updated user profile"
+      redirect_to user_path
     else render 'edit'
+    end
+  end
   
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :area, :profile, :password_confirmation)
   end
-    
 end
