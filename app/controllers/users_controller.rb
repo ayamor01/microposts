22a5f4ab767b5868b9c789a5aae6f4c1484b2f05
crop0422 <ayumi.yamori@gmail.com>
2015-11-19 04:45:02 +0000

    finished challenge1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(id: params[:id])
+    if @user == nil
+      flash[:danger] = "Couldn't find user"
+      redirect_to root_path
+    end
   end
   
   def new
@@ -24,7 +28,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:success] = "Updated user profile"
+      flash[:success] = "Updated user information"
       redirect_to user_path
     else render 'edit'
     end
