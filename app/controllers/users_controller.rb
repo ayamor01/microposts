95650d9330e6da2e0108838faffcb808d4d7c254
crop0422 <ayumi.yamori@gmail.com>
2015-11-24 10:20:18 +0000

    challenge2 3rd try

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,21 +25,24 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
     if @user == current_user
-      render 'edit'
-    else redirect_to root_url
+       render 'edit'
+    else
+       redirect_to root_url
     end
   end
   
   def update
     @user = User.find(params[:id])
-      if @user == current_user
-        if @user.update(user_params)
-          flash[:success] = "Updated user information"
-          redirect_to user_path(current_user)
-        else render 'edit'
-        end
-      else redirect_to root_url
+    if @user == current_user
+      if @user.update(user_params)
+         flash[:success] = "Updated user information"
+         redirect_to user_path(current_user)
+      else
+         render 'edit'
       end
+    else
+       redirect_to root_url
+    end
   end
   
   def followings
